9fa20117a1c4340bcf9ede90b011abd258d84c08
vadim <vadim@debian-vadim>
2025-07-16 13:07:03 +0500

    added localize

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::path::PathBuf;
-// use std::collections::HashMap;
+use std::collections::HashMap;
 use std::sync::Arc;
+use once_cell::sync::Lazy;
+use rust_i18n::t;
+use locale_config::Locale;
 
 use clap::Parser;
 use colored::*;
@@ -14,9 +17,19 @@ use tokio::io::{AsyncWriteExt, AsyncBufReadExt, BufReader};
 use tokio::sync::Semaphore;
 use tokio::task;
 // use url::Url;
+// as
+
+rust_i18n::i18n!("i18n");
+
+
+static INIT_LOCALE: Lazy<()> = Lazy::new(|| {
+    let system_locale = Locale::user_default().to_string();
+    let short_locale = system_locale.split('_').next().unwrap_or("en");
+    rust_i18n::set_locale(short_locale);
+});
 
 #[derive(Parser)]
-#[command(name = "dwrs",author, version, about = "Parallel downloader with progress bar")]
+#[command(name = "dwrs",author, version, about = format!("{}",t!("about")))]
 #[command(group(clap::ArgGroup::new("input").required(true).args(&["url","file"])))]
 struct Args {
 
@@ -43,6 +56,8 @@ struct Args {
 //     }
 // }
 
+
+
 async fn parse_file(path: &PathBuf) -> Result<Vec<(String, String)>, Box<dyn std::error::Error>> {
     let file = File::open(path).await?;
     let reader = BufReader::new(file);
@@ -58,7 +73,7 @@ async fn parse_file(path: &PathBuf) -> Result<Vec<(String, String)>, Box<dyn std
             let filename = parts[0].split('/').last().unwrap_or("file.bin").to_string();
             pairs.push((parts[0].to_string(), filename));
         } else {
-            eprintln!("Wrong format of string: {}", line)
+            eprintln!("{}: {}", t!("wrong-format-string"), line)
         }
     }
     Ok(pairs)
@@ -66,8 +81,11 @@ async fn parse_file(path: &PathBuf) -> Result<Vec<(String, String)>, Box<dyn std
 
 #[tokio::main]
 async fn main() {
+    Lazy::force(&INIT_LOCALE);
     env_logger::init();
     info!("logger init");
+
+
 
     let args = Args::parse();
 
@@ -75,7 +93,7 @@ async fn main() {
 
     if let Some(file_path) = args.file {
         url_output_pairs = parse_file(&file_path).await.unwrap_or_else(|e| {
-            eprintln!("Error in reading file: {}", e);
+            eprintln!("{}: {}", t!("error-in-reading-file"), e);
             panic!();
         });
     } else {
@@ -92,7 +110,7 @@ async fn main() {
         if !args.output.is_empty() && args.output.len() != args.url.len() {
             println!("{}",args.output.len());
             error!("Error count of output files dont equal of urls");
-            eprintln!("{}","Error count of output files dont equal of urls".bold().red());
+            eprintln!("{}",t!("error-count"));
             panic!()
         }
 
@@ -109,12 +127,8 @@ async fn main() {
     let mut tasks = FuturesUnordered::new();
 
     for (url, output) in url_output_pairs.into_iter() {
+        let outstr = output.clone();
         let output = PathBuf::from(output);
-        // let output = if let Some(path) = args.output.get(i) {
-        //     PathBuf::from(path)
-        // } else {
-        //     PathBuf::from(get_filename_from_url(&url).unwrap())
-        // };
 
         let client = client.clone();
         let mp = mp.clone();
@@ -128,11 +142,11 @@ async fn main() {
                 ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos:>7}/{len:7} ({percent}%) {msg}").unwrap()
             );
 
-            pb.set_message(format!("Download {} -> {:?}",url, output));
+            pb.set_message(format!("{} {} → {}", t!("download"), &url, outstr));
 
             match download_file(&client, &url , &output, &pb).await {
-                Ok(_) => pb.finish_with_message(format!("{}: {:?}","Success dowloaded".green().bold(),output)),
-                Err(e) => pb.finish_with_message(format!("{}: {:?}: {}","Error download".red().bold(),output,e)),
+                Ok(_) => pb.finish_with_message(format!("{}: {}", t!("download-finish"), &outstr)),
+                Err(e) => pb.finish_with_message(format!("{}: {}: {}", t!("download-error"), &outstr, e)),
             }
         }));
     }
